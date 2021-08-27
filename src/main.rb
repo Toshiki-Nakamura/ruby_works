68b7b6e1edab7b4fe7b844720d10aeca64b72ff8
Toshiki　Nakamura <t.jack0624@gmail.com>
2021-08-27 16:23:04 +0900

    init

---
+++ src/main.rb
@@ -0,0 +1,26 @@
+greet = "\033[35mHello, my first ruby program\033[0m"
+puts greet
+puts
+
+puts "======= Int Array ======="
+arr = [42, 21, 90, 24]
+puts "arr: "
+print arr
+puts
+
+sorted_arr = arr.sort
+puts "after sort:"
+print sorted_arr
+puts
+
+puts "======= String ======="
+string = "tnakamur"
+
+puts "string: "
+puts string
+puts "string.index('a'): "
+puts string.index("a")
+puts "string.length: "
+puts string.length
+puts "string.class:"
+puts string.class
