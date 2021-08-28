df09e8708e57b1338547b13109e552da2fbe4770
Toshiki　Nakamura <t.jack0624@gmail.com>
2021-08-28 13:27:22 +0900

    Class, module

--- src/Class.rb
+++ src/Class.rb
@@ -41,3 +41,69 @@ obj2.setAddr("Roppongi")
 puts obj2.getName()
 puts obj2.getOrg()
 puts obj2.getAddr()
+
+
+puts "=== Nest Class ==="
+puts "nest Class"
+class Foo
+	class Bar
+		def foobar
+			puts "Bar of Foo"
+		end
+	end
+end
+
+nest = Foo::Bar.new
+puts nest.foobar()
+
+class Foo
+	class Bar
+		def foobar
+			puts "Bar of Foo"
+		end
+	end
+	class Hoge
+		def hoge
+			puts "hoge of Foo"
+		end
+	end
+end
+puts Foo::Hoge.new.hoge()
+
+
+puts "=== Special Class ==="
+class Lang
+	def cpp
+		puts "1. C/C++"
+	end
+end
+obj3 = Lang.new
+class << obj3
+	def ruby
+		puts "2. Ruby"
+	end
+end
+obj3.ruby
+
+puts "=== module ==="
+
+module Mdl
+	def add(x, y)
+		return x + y
+	end
+end
+module Mdl2
+	def show(n)
+		print "My LEVEL: ", 42 , "\n"
+	end
+end
+
+class C
+	include Mdl
+	include Mdl2
+end
+
+mdl = C.new
+
+puts mdl.add(21, 19)
+mdl.show(42)
